770b14987764aed44fc7dd6d9e833d22d7efaf8b
Yufan CHEN <108385820+CYF2000127@users.noreply.github.com>
2025-10-10 02:56:25 +0800

    Add files via upload

---
+++ pdf_extraction.py
@@ -0,0 +1,34 @@
+import os
+import json
+import argparse
+from huggingface_hub import hf_hub_download
+from visualheist.methods import batch_pdf_to_figures_and_tables,_pdf_to_figures_and_tables
+
+
+def load_config(config_file):
+    """Load configurations from JSON file with path adjustments"""
+    with open(config_file, 'r') as f:
+        config = json.load(f)
+    
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    config['default_image_dir'] = os.path.join(base_dir, config.get('default_image_dir', ''))
+    config['default_json_dir'] = os.path.join(base_dir, config.get('default_json_dir', ''))
+    config['default_graph_dir'] = os.path.join(base_dir, config.get('default_graph_dir', ''))
+    return config
+
+
+def run_pdf(config_file=None, pdf_dir=None, image_dir=None, model_size=None):
+    """Main function to run VisualHeist processing"""
+    if config_file:
+        config = load_config(config_file)
+    else:
+        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        default_config = os.path.join(base_dir, 'scripts/startup.json')
+        config = load_config(default_config) if os.path.exists(default_config) else {}
+    
+    image_dir = image_dir or config.get('image_dir', config.get('default_image_dir'))
+    model_size = model_size or config.get('model_size', "base")
+    
+    print(f"\nProcessing the PDF: {pdf_dir}")
+    print(f"Using {'LARGE' if model_size == 'large' else 'BASE'} model")
+    _pdf_to_figures_and_tables(pdf_dir, image_dir, large_model=(model_size == "large"))
